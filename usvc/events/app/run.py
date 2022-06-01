f3eff33d150dae31d8a81bc925473566f754592e
stevenli2020 <stevenli2020@github.com>
2022-06-01 11:00:17 +0800

    update

--- usvc/events/app/run.py
+++ usvc/events/app/run.py
@@ -11,25 +11,32 @@ brokerAddress="vernemq"
 clientID="0001"
 userName="events"
 userPassword="3e90edbe0f6cbef1cec300719e684a0e5de648a7"
-
+dataBuffer=[]
 
 
 # print(int("1ffe",16))
 # exit()
 
 
-
 def on_message(mosq, obj, msg):
-    print(msg.topic + " - " + str(msg.payload)[2:-1])
+    in_data = str(msg.payload)[2:-1]
+    print(msg.topic + " - " + in_data)
     TOPIC=msg.topic.split("/")
-    MSG=(str(msg.payload)[2:-1]).split(":")
+    if "/STATUS" not in msg.topic:
+        if in_data in dataBuffer:
+            return
+        else:
+            dataBuffer.append(in_data)
+            if len(dataBuffer)>256:
+                del dataBuffer[0] 
+    MSG=(in_data).split(":")
     db = mysql.connector.connect(
       host="database",
       user="usvc",
       password="O0zg2mbfs9!VQbM-",
       database="sensor_db"
     )    
-    cursor = db.cursor() 
+    cursor = db.cursor(dictionary=True)  
     if TOPIC[2]!="USVC" and TOPIC[3]=="E":
         COORDINATOR_ID=TOPIC[2]
         CLUSTER_ID=TOPIC[4]
@@ -38,11 +45,20 @@ def on_message(mosq, obj, msg):
         NODE_TYPE=MSG[0]
         NODE_BATT=int(MSG[2], 16)
         NODE_DATA=int(MSG[3], 16)
+        if NODE_TYPE=="03":
+            if NODE_DATA==0:
+                NODE_DATA=1
+            sql="SELECT * FROM `EVENTS` WHERE `NODE_ID`='"+NODE_ID+"' ORDER BY `ID` DESC LIMIT 1;";
+            cursor.execute(sql)
+            R = cursor.fetchone()
+            EVENT_ID_LAST = R['EVENT_ID']
+            if (EVENT_ID-EVENT_ID_LAST) > NODE_DATA:
+                NODE_DATA = EVENT_ID - EVENT_ID_LAST
         print(str(EVENT_ID)+" - "+str(NODE_TYPE)+","+str(NODE_BATT)+","+str(NODE_DATA))            
-        sql = "INSERT INTO `EVENTS` (`CLUSTER_ID`, `NODE_ID`, `TYPE`, `DATA_TYPE`, `EVENT_ID`, `BAT_LVL`, `SENSOR_DATA`) VALUES ('"+CLUSTER_ID+"','"+NODE_ID+"','E','"+NODE_TYPE+"',"+str(EVENT_ID)+","+str(NODE_BATT)+","+str(NODE_DATA)+")"
+        sql = "INSERT INTO `EVENTS` (`COORDINATOR`, `CLUSTER_ID`, `NODE_ID`, `TYPE`, `DATA_TYPE`, `EVENT_ID`, `BAT_LVL`, `SENSOR_DATA`) VALUES ('"+COORDINATOR_ID+"','"+CLUSTER_ID+"','"+NODE_ID+"','E','"+NODE_TYPE+"',"+str(EVENT_ID)+","+str(NODE_BATT)+","+str(NODE_DATA)+")"
         cursor.execute(sql)
         # db.commit()
-        sql = "UPDATE `NODES` SET `CLUSTER_ID`='"+CLUSTER_ID+"',`SENSOR_TYPE`='"+NODE_TYPE+"',`PARENT`='"+COORDINATOR_ID+"',`BATT_LVL`="+str(NODE_BATT)+",`SENSOR_DATA`="+str(NODE_DATA)+" WHERE `MAC`='"+NODE_ID+"'"
+        sql = "UPDATE `NODES` SET `EVENT_ID`="+str(EVENT_ID)+",`CLUSTER_ID`='"+CLUSTER_ID+"',`SENSOR_TYPE`='"+NODE_TYPE+"',`PARENT`='"+COORDINATOR_ID+"',`BATT_LVL`="+str(NODE_BATT)+",`SENSOR_DATA`="+str(NODE_DATA)+" WHERE `MAC`='"+NODE_ID+"'"
         # print(sql)
         cursor.execute(sql)
         db.commit()
@@ -60,7 +76,7 @@ def on_message(mosq, obj, msg):
         cursor.execute(sql)        
         db.commit() 
     elif TOPIC[2]=="USVC":
-        sql = "INSERT INTO `STATUS_EVENTS` (`TIME`, `NAME`, `EVENT`) VALUES (NOW(),'"+TOPIC[2]+"','"+str(msg.payload)[2:-1]+"');"
+        sql = "INSERT INTO `STATUS_EVENTS` (`TIME`, `NAME`, `EVENT`) VALUES (NOW(),'"+TOPIC[2]+"-"+TOPIC[3]+"','"+(str(msg.payload)[2:-1]).replace("'","")+"');"
         cursor.execute(sql)        
         db.commit()        
     cursor.close()
